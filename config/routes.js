125d46cda939dc13adff8548be27a55e9b5e9b00
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-23 12:27:47 +0800

    routes update

--- config/routes.js
+++ config/routes.js
@@ -235,16 +235,6 @@ router.param('movie_id', function(req, res, next, movie_id) {
   });
 });
 
-var isRevokedCallback = function(req, payload, done){
-  var issuer = payload.iss;
-  var tokenId = payload.jti;
-
-  data.getRevokedToken(issuer, tokenId, function(err, token){
-    if (err) { return done(err); }
-    return done(null, !!token);
-  });
-};
-
 router.get('/logout', function (req, res) {
   // res.send(req.user);
 
